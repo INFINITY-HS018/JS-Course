0c5fc487ac424ecf43778f1cc833d64a48991937
INFINITY-HS018 <163451990+INFINITY-HS018@users.noreply.github.com>
2024-06-01 14:56:28 +0000

    01_variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,8 +2,18 @@ const accountId = 23223
 let accountEmail = "harsh@gmail.com"
 var accountPassword = "1234"
 accountCity = "Kanpur"
+let accountState;
 
 // accountId = 23 // not allowed
 
+accountEmail = "newemail@gmail.com"
+accountPassword = "4321"
+accountCity = "Lucknow"
 
-console.log(accountId);
+/*
+Prefer not to use var
+because problem in block scope and functional scope
+*/
+
+console.log(accountId,accountEmail);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
